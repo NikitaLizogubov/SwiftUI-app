b5a231a8141d5560ef546c0c576a6ad756926d92
Nikita Lizogubov <nikitalizogubov2000@gmail.com>
2022-10-15 23:08:25 +0300

    init project

---
+++ SwiftUI app/SwiftUI app/SwiftUI_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_appApp.swift
+//  SwiftUI app
+//
+//  Created by Nikita Lizogubov on 15.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
